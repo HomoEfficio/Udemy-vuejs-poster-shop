440340f9092e3c961aac02ba53302f9a23a55c33
HomoEfficio <homo.efficio@gmail.com>
2017-05-05 23:54:03 +0900

    Poster 프로젝트 Search Button 까지 - 32강 까지

--- public/script.js
+++ public/script.js
@@ -1,1 +1,61 @@
-console.log('It works.');
+var PRICE = 9.99;
+
+new Vue({
+    el: '#app',
+    data: {
+        total: 0,
+        items: [
+            { id: 1, title: 'Item 1'},
+            { id: 2, title: 'Item 2'},
+            { id: 3, title: 'Item 3'},
+        ],
+        cart: [],
+        search: ''
+    },
+    methods: {
+        onSubmit: function() {
+            console.log(this.search);
+        },
+        addItem: function(index) {
+            this.total += PRICE;
+            var item = this.items[index];
+            var found = false;
+            for (var i = 0, len = this.cart.length; i < len; i++) {
+                if (this.cart[i].id === item.id) {
+                    this.cart[i].qty++;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                this.cart.push({
+                    id: item.id,
+                    title: item.title,
+                    price: PRICE,
+                    qty: 1
+                });
+            }
+        },
+        inc: function(item) {
+            item.qty++;
+            this.total += item.price;
+        },
+        dec: function(item) {
+            item.qty--;
+            this.total -= item.price;
+            if (item.qty <= 0) {
+                for (var i = 0, len = this.cart.length; i < len ; i++) {
+                    if (this.cart[i].id === item.id) {
+                        this.cart.splice(i, 1);
+                        break;
+                    }
+                }
+            }
+        }
+    },
+    filters: {
+        currency: function(price) {
+            return '$'.concat(price.toFixed(2));
+        }
+    }
+});
