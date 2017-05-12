310d6206a4177a70a83a3f744ead293de7914b93
Oh Myung Woon <omwomw@sk.com>
2017-05-12 21:57:15 +0900

    reco action 용 payload 구성

--- public/script.js
+++ public/script.js
@@ -1,23 +1,65 @@
-var PRICE = 9.99;
 var LOAD_NUM = 10;
+var RECO_SERVICE_ID = '543';
+var RECO_REF = 'http://dev.recopick.com/index.html';
+var RECO_URL = 'http://dev.recopick.com/index.html';
+var RECO_UID = (function() {
+    var url = document.location.href;
+    var paramString = url.substring(url.indexOf('?') + 1);
+    var params = paramString.split('&');
+    for (var i = 0, len = params.length; i < len; i++) {
+        var param = params[i];
+        if (param.trim().indexOf('uid') === 0)
+            return param.substring(param.indexOf('=') + 1);
+    }
+    return '';
+}());
+var RECO_MID = Math.floor(Math.random()*(1000000000-100000000)+100000000);
+var RECO_BIRTHYEAR = Math.floor(Math.random()*(2000-1930)+1930);
+var RECO_GENDER = Math.floor(Math.random()*(1-100)+100) % 2 ? 'F' : 'M';
 
 new Vue({
     el: '#app',
+    mounted: function() {
+        if (RECO_UID.length === 0) {
+            this.hasNoUID = true;
+        }
+        this.sendLog('visit', {
+            service_id: RECO_SERVICE_ID,
+            uid: RECO_UID,
+            ref: RECO_REF,
+            url: RECO_URL,
+            user: {
+                mid: RECO_MID,
+                gender: RECO_GENDER,
+                birthyear: RECO_BIRTHYEAR
+            }
+        });
+
+        this.onSearch();
+
+        var vueInstance = this;
+        var elem = document.getElementById('product-list-bottom');
+        var watcher = scrollMonitor.create(elem);
+        watcher.enterViewport(function() {
+            vueInstance.appendItems();
+        });
+    },
     data: {
+        hasNoUID: false,
         total: 0,
         items: [],
         cart: [],
         results: [],
         newSearch: 'anime',
         lastSearch: '',
-        loading: false,
-        price: PRICE
+        loading: false
     },
     computed: {
         noMoreItems: function() {
             return this.items.length == this.results.length && this.results.length > 0
         }
     },
+
     methods: {
         appendItems: function() {
             if (this.items.length < this.results.length) {
@@ -32,6 +74,7 @@ new Vue({
                 this.$http.get('/search/'.concat(this.newSearch))
                     .then(
                         function(res) {
+                            console.log(res);
                             this.lastSearch = this.newSearch;
                             this.results = res.data;
                             this.appendItems();
@@ -40,41 +83,53 @@ new Vue({
                         function() {}
                     );
                 this.sendLog('search', {
-                    service_id: '543',
-                    uid: '33333',
-                    ref: 'http://example.com',
-                    url: 'http://example.com/product/detail.html',
+                    service_id: RECO_SERVICE_ID,
+                    uid: RECO_UID,
+                    ref: RECO_REF,
+                    url: RECO_URL,
                     q: this.newSearch,
                     user: {
-                        mid: 'BASE64ENCODED',
-                        gender: 'M',
-                        birthyear: 2010
+                        mid: RECO_MID,
+                        gender: RECO_GENDER,
+                        birthyear: RECO_BIRTHYEAR
                     }
                 });
             }
         },
         onDetail: function(i) {
-            console.log(this.items[i].title);
+            var item = this.items[i];
+            // id, title, image, price, currency,
+            // description, author, sale_price, sale_currency, is_hidden,
+            // c1, c2, c3, brand, bc1
             this.sendLog('view', {
-                service_id: '543',
-                uid: '33333',
-                ref: 'http://example.com',
-                url: 'http://example.com/product/detail.html',
-                q: this.newSearch,
+                service_id: RECO_SERVICE_ID,
+                uid: RECO_UID,
+                ref: RECO_REF,
+                url: RECO_URL,
+                items: [{
+                    id: item.id,
+                    title: item.title,
+                    image: item.link,
+                    price: item.score,
+                    currency: item.type,
+                    description: item.description,
+                    c1: item.section,
+                    c2: item.topic
+                }],
                 user: {
-                    mid: 'BASE64ENCODED',
-                    gender: 'M',
-                    birthyear: 2010
+                    mid: RECO_MID,
+                    gender: RECO_GENDER,
+                    birthyear: RECO_BIRTHYEAR
                 }
             });
         },
         addToCart: function(index) {
-            this.total += PRICE;
             var item = this.items[index];
+            this.total += item.score;
             var found = false;
             for (var i = 0, len = this.cart.length; i < len; i++) {
                 if (this.cart[i].id === item.id) {
-                    this.cart[i].qty++;
+                    this.cart[i].count++;
                     found = true;
                     break;
                 }
@@ -83,62 +138,73 @@ new Vue({
                 this.cart.push({
                     id: item.id,
                     title: item.title,
-                    price: this.price,
-                    qty: 1
+                    image: item.link,
+                    price: item.score,
+                    currency: item.type,
+                    description: item.description,
+                    c1: item.section,
+                    c2: item.topic,
+                    count: 1
                 });
             }
             this.sendLog('basket', {
-                service_id: '543',
-                uid: '33333',
-                ref: 'http://example.com',
-                url: 'http://example.com/product/detail.html',
-                items: [
-                    {
-                        id: '888',
-                        count: 3,
-                        c1: '스포츠',
-                        c2: '의류',
-                        total_sales: 13000,
-                        order_no: '1234'
-                    },
-                    {
-                        id: '999',
-                        count: 2,
-                        c1: '가정',
-                        c2: '육아용품',
-                        c3: '기저귀',
-                        total_sales: 25000,
-                        order_no: '456'
-                    }
-                ],
+                service_id: RECO_SERVICE_ID,
+                uid: RECO_UID,
+                ref: RECO_REF,
+                url: RECO_URL,
+                items: this.cart,
                 user: {
-                    mid: 'BASE64ENCODED',
-                    gender: 'M',
-                    birthyear: 2010
+                    mid: RECO_MID,
+                    gender: RECO_GENDER,
+                    birthyear: RECO_BIRTHYEAR
                 }
             });
         },
         inc: function(i) {
             var current = this.cart[i];
-            current.qty++;
+            current.count++;
             this.total += current.price;
             this.sendLog('basket', { msg: 'inc' });
         },
         dec: function(i) {
             var current = this.cart[i];
-            current.qty--;
+            current.count--;
             this.total -= current.price;
-            if (current.qty <= 0) {
+            if (current.count <= 0) {
                 this.cart.splice(i, 1);
             }
             this.sendLog('basket', { msg: 'dec' });
         },
         onOrder: function(cart) {
-            console.log(cart);
-            this.sendLog('order', { msg: 'ordered' });
+            this.convertToOrderItems(cart);
+            this.sendLog('order', {
+                service_id: RECO_SERVICE_ID,
+                uid: RECO_UID,
+                ref: RECO_REF,
+                url: RECO_URL,
+                items: this.cart,
+                user: {
+                    mid: RECO_MID,
+                    gender: RECO_GENDER,
+                    birthyear: RECO_BIRTHYEAR
+                }
+            });
+        },
+        getRecoDataFrom: function(results) {
+            for (var i = 0, len = results.length; i < len; i++) {
+                results[i].count = results[i].comment_count;
+
+            }
         },
         sendLog: function(action, payload) {
-            console.log(action);
+            console.log(action, payload);
+        },
+        convertToOrderItems: function(cartItems) {
+            for (var i = 0, len = cartItems.length; i < len; i++) {
+                var item = cartItems[i];
+                item.total_sales = item.price * item.count;
+                item.order_no = Math.floor(Math.random()*(10000000000-1000000000)+1000000000).toString();
+            }
         }
     },
     filters: {
@@ -146,26 +212,5 @@ new Vue({
             return '$'.concat(price.toFixed(2));
         }
     },
-    mounted: function() {
-        this.sendLog('visit', {
-            service_id: '543',
-            uid: '33333',
-            ref: 'http://example.com',
-            url: 'http://example.com/product/detail.html',
-            user: {
-                mid: 'BASE64ENCODED',
-                gender: 'M',
-                birthyear: 2010
-            }
-        });
 
-        this.onSearch();
-
-        var vueInstance = this;
-        var elem = document.getElementById('product-list-bottom');
-        var watcher = scrollMonitor.create(elem);
-        watcher.enterViewport(function() {
-            vueInstance.appendItems();
-        });
-    }
 });
